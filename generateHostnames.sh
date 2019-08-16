a97e7fb57adde7d5455b7a864244b4b21dba9a64
rohan <rohan@rmolloy.io>
2019-08-17 03:56:36 +1200

    release

--- generateHostnames.sh
+++ generateHostnames.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# File containing list of words (seperated by NL)
-wordList=${1:-'words.txt'}
-# Define the network range printed
-subnet='192.168'
-netwMin=0
-netwMax=2
-hostMin=2
-hostMax=250
+
+# Define the network range
+subnet=${subnet:-192.168}
+netwMin=${netwMin:-122}
+netwMax=${netWMax:-125}
+hostMin=${hostMin:-130}
+hostMax=${hostMax:-251}
+
 # Iterate and print
-readarray words < <(printf "%s%s\n" $(shuf $wordList))
-let "z=0"; 
-for ((i=$netwMin; i<$netwMax; i++)); do   
+wordlist=${wordlist:-'words.txt'}
+words=( `printf "%s%s " $(shuf $wordlist)` );
+let "z=0";
+for ((i=$netwMin; i<$netwMax; i++)); do
   for ((y=$hostMin; y<$hostMax; y++)); do
-   printf "%s\t%s" "$subnet.$i.$y" "${words[$z]}";    
-   let "z++"; 
-   done; 
+    printf "%s %s\n" "$subnet.$i.$y" "${words[$z]}";
+    let "z++";
+  done;
 done;
+
+    
+
